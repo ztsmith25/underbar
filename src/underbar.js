fd951d76adfe3c9baa8f5746bf2e4e0514dd4538
Zachary T. Castellanos-Smith <ztsmith25@gmail.com>
2015-07-13 18:16:00 -0700

    Implemented reject

--- src/underbar.js
+++ src/underbar.js
@@ -93,6 +93,8 @@
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var falseTest = function (element) {return !(test(element))};
+    return _.filter(collection, falseTest)
   };
 
   // Produce a duplicate-free version of the array.
